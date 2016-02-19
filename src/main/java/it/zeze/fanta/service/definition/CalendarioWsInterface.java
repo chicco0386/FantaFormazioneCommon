ac8e3fbab63187c1ae6504b6fa83ae5cfca367ba
enrico <enrico@ic1400833>
2016-02-19 15:33:31 +0100

    First commit

---
+++ src/main/java/it/zeze/fanta/service/definition/CalendarioWsInterface.java
@@ -0,0 +1,5 @@
+package it.zeze.fanta.service.definition;
+
+public interface CalendarioWsInterface {
+	public void inizializzaCalendario();
+}
---
+++ src/main/java/it/zeze/fanta/service/definition/GiocatoriWsInterface.java
@@ -0,0 +1,6 @@
+package it.zeze.fanta.service.definition;
+
+public interface GiocatoriWsInterface {
+	public void unmarshallAndSaveFromHtmlFile(boolean noLike);
+	public void unmarshallAndSaveFromHtmlFileForUpdateStagione(boolean noLike);
+}
