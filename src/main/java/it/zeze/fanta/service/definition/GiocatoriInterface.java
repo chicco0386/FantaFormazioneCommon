b3e3c3d833109623fa1cab8901b6b58a829fa05e
enrico <enrico@ic1400833>
2016-03-01 11:32:29 +0100

    Fatto probabili formazioni

--- src/main/java/it/zeze/fanta/service/definition/GiocatoriInterface.java
+++ src/main/java/it/zeze/fanta/service/definition/GiocatoriInterface.java
@@ -7,4 +7,5 @@ public interface GiocatoriInterface {
 	public void unmarshallAndSaveFromHtmlFileForUpdateStagione(boolean noLike);
 	public Giocatori getGiocatoreByNomeSquadraRuolo(String nomeGiocatore, String squadra, String ruolo, String stagione, boolean noLike);
 	void insertOrUpdateGiocatore(String nomeSquadra, String nomeGiocatore, String ruolo, String stagione, boolean noLike);
+	Giocatori getGiocatoreByNomeSquadra(String nomeGiocatore, String squadra, String stagione, boolean noLike);
 }
