5368157bc0a0edb725caf8360282946e555b69f0
enrico <enrico@ic1400833>
2016-06-20 17:31:14 +0200

    Vado avanti con la chiamate ad ejb da seam

--- src/main/java/it/zeze/fanta/service/definition/ejb/proxy/seam/GiornateSeamRemote.java
+++ src/main/java/it/zeze/fanta/service/definition/ejb/proxy/seam/GiornateSeamRemote.java
@@ -1,8 +1,33 @@
 package it.zeze.fanta.service.definition.ejb.proxy.seam;
 
+import java.util.List;
+
 import javax.ejb.Remote;
+
+import it.zeze.fanta.service.bean.SelectItem;
+import it.zeze.fantaformazioneweb.entity.Giornate;
 
 @Remote
 public interface GiornateSeamRemote {
+
+	void unmarshallAndSaveFromHtmlFile(String stagione);
+
+	String getStagione(String stagioneInput);
+
+	int getIdGiornata(int numeroGiornata, String stagione);
+
+	Giornate getGiornataById(int idGiornata);
+
+	int getIdGiornata(String dataString);
+
+	Giornate getGiornata(String dataString);
+
+	Giornate getLastGiornata();
+
+	List<Giornate> getGiornateByStagione(String stagione);
+
+	List<Giornate> getGiornateAll();
+
+	List<SelectItem> getStagioniAll();
 
 }
--- src/main/java/it/zeze/fanta/service/definition/ejb/proxy/seam/SquadreSeamRemote.java
+++ src/main/java/it/zeze/fanta/service/definition/ejb/proxy/seam/SquadreSeamRemote.java
@@ -2,7 +2,19 @@ package it.zeze.fanta.service.definition.ejb.proxy.seam;
 
 import javax.ejb.Remote;
 
+import it.zeze.fantaformazioneweb.entity.Squadre;
+
 @Remote
 public interface SquadreSeamRemote {
+
+	void unmarshallAndSaveFromHtmlFile();
+
+	void initMappaSquadre();
+
+	Squadre getSquadraFromMapByNome(String nomeSquadraToSearch);
+
+	Squadre getSquadraByNome(String nomeSquadraToSearch);
+
+	Squadre getSquadraById(int idSquadra);
 
 }
