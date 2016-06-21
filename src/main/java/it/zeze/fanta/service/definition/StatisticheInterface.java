53e904fc7861d8ce270b4bfc6be37933b5ccf799
enrico <enrico@ic1400833>
2016-06-21 13:13:29 +0200

    Vado avanti con la chiamate ad ejb da seam

--- src/main/java/it/zeze/fanta/service/definition/StatisticheInterface.java
+++ src/main/java/it/zeze/fanta/service/definition/StatisticheInterface.java
@@ -1,6 +1,10 @@
 package it.zeze.fanta.service.definition;
 
+import it.zeze.fantaformazioneweb.entity.Statistiche;
+
 public interface StatisticheInterface {
 	
 	public void unmarshallAndSaveFromHtmlFile(String stagione);
+
+	Statistiche getStatisticheIdGiocatoreIdGiornata(int idGiocatore, int idGiornata);
 }
--- src/main/java/it/zeze/fanta/service/definition/ejb/proxy/seam/StatisticheSeamRemote.java
+++ src/main/java/it/zeze/fanta/service/definition/ejb/proxy/seam/StatisticheSeamRemote.java
@@ -2,9 +2,13 @@ package it.zeze.fanta.service.definition.ejb.proxy.seam;
 
 import javax.ejb.Remote;
 
+import it.zeze.fantaformazioneweb.entity.Statistiche;
+
 @Remote
 public interface StatisticheSeamRemote {
 
 	void unmarshallAndSaveFromHtmlFile(String stagione);
+
+	Statistiche getStatisticheIdGiocatoreIdGiornata(int idGiocatore, int idGiornata);
 
 }
